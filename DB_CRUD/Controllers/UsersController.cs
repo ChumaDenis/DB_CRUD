17e6df69129b10213268bef0d4d09ac7df3b544e
Denis chuma <chumiks1999@gmail.com>
2023-05-11 18:13:21 +0300

    Add UsersController and view

---
+++ DB_CRUD/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DB_CRUD.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
