1521a3d3a1b41bb14de950524ed273b8062d2bfb
Denis chuma <chumiks1999@gmail.com>
2023-05-11 20:41:04 +0300

    Add Create to OrdersController

--- DB_CRUD/Controllers/OrdersController.cs
+++ DB_CRUD/Controllers/OrdersController.cs
@@ -1,4 +1,5 @@
 ﻿using DB_CRUD.Contexts;
+using DB_CRUD.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,12 +14,39 @@ namespace DB_CRUD.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View(_context.Orders);
+
+            return View(await _context.Orders.Include(o => o.User).ToListAsync());
         }
 
 
+        public async Task<IActionResult> Create()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("UserID,OrderDate,OrderCost,ItemsDescription,ShippingAddress")] Order order)
+        {
+            try
+            {
+                
+                _context.Orders.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
 
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
