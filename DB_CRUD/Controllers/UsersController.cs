6e5df73fb20c35c362d6d4fd7a1cae8e2ec3f76d
Denis chuma <chumiks1999@gmail.com>
2023-05-11 18:56:19 +0300

    Add DbContext to UsersController

--- DB_CRUD/Controllers/UsersController.cs
+++ DB_CRUD/Controllers/UsersController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DB_CRUD.Contexts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DB_CRUD.Controllers
 {
     public class UsersController : Controller
     {
+        private CrudDbContext _context;
+
+        public UsersController(CrudDbContext context) 
+        {
+            _context= context;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Users);
         }
     }
 }
