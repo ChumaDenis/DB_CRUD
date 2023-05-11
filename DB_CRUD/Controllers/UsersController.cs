a43d7047dcc63f5836dc1d41b6e0536c711528a5
Denis chuma <chumiks1999@gmail.com>
2023-05-11 19:17:52 +0300

    Add Create to UsersController

--- DB_CRUD/Controllers/UsersController.cs
+++ DB_CRUD/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using DB_CRUD.Contexts;
+using DB_CRUD.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DB_CRUD.Controllers
@@ -7,15 +8,55 @@ namespace DB_CRUD.Controllers
     {
         private CrudDbContext _context;
 
-        public UsersController(CrudDbContext context) 
+        public UsersController(CrudDbContext context)
         {
-            _context= context;
+            _context = context;
         }
 
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             return View(_context.Users);
         }
+
+        public async Task<IActionResult> Create()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Login,Password,PasswordConfirmation, FirstName, LastName, DateOfBirth, Gender")] UserDTO user)
+        {
+            try
+            {
+                User newUser=new User() 
+                { FirstName= user.FirstName, LastName=user.LastName,
+                    Login=user.Login, Password=user.Password,
+                    DateOfBirth=user.DateOfBirth,Gender=user.Gender
+                };
+                _context.Users.Add(newUser);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ DB_CRUD/Models/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DB_CRUD.Models
+{
+    public class UserDTO
+    {
+        public int UserID { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        [Required, Compare("Password")]
+        public string PasswordConfirmation { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
