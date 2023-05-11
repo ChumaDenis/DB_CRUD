46ca6062ead7ff35344e16dd85eff3e8ce1fa3e6
Denis chuma <chumiks1999@gmail.com>
2023-05-11 20:23:33 +0300

    Add Orders

--- DB_CRUD/Controllers/OrdersController.cs
+++ DB_CRUD/Controllers/OrdersController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DB_CRUD.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DB_CRUD.Controllers
 {
     public class OrdersController : Controller
     {
+        private CrudDbContext _context;
+
+        public OrdersController(CrudDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Orders);
         }
+
+
+
     }
 }
